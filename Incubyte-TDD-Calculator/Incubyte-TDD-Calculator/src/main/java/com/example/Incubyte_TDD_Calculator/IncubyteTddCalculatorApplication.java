b3692690bfb6e5b3ade4ddf8c9478349d8f74410
Ansh-Hathi <anshhathi9@gmail.com>
2025-07-04 22:19:30 +0530

    [Init] Setup Maven Project & JUnit

---
+++ Incubyte-TDD-Calculator/Incubyte-TDD-Calculator/src/main/java/com/example/Incubyte_TDD_Calculator/IncubyteTddCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Incubyte_TDD_Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteTddCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteTddCalculatorApplication.class, args);
+	}
+
+}
---
+++ Incubyte-TDD-Calculator/Incubyte-TDD-Calculator/src/test/java/com/example/Incubyte_TDD_Calculator/IncubyteTddCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Incubyte_TDD_Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteTddCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
